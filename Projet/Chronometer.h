a3da68fd92a0b1c35c8ce40b95fc3d6727f9f69f
Taryck CIDERE <tar.cidere@gmail.com>
2024-12-04 20:51:33 +0100

    Ajout d'un Chronometre

--- Projet/Chronometer.h
+++ Projet/Chronometer.h
@@ -7,7 +7,20 @@
 typedef struct Chronometer {
     clock_t start;
     clock_t end;
+    char* label;
 } Chronometer;
+
+void setLabel(Chronometer* chrono, char* label) {
+    if (chrono) {
+        chrono->label = label;
+    }
+}
+
+char* getLabel(Chronometer* chrono) {
+    if (chrono) {
+
+    }
+}
 
 void startChronometer(Chronometer* chrono);
 void EndChronometer(Chronometer* chrono);
@@ -16,4 +29,4 @@ void getElapsedTime(Chronometer* chrono);
 
 void writeElapsedTimeToCSV(Chronometer* chrono, const char* filename, const char* label);
 
-#endif // CHRONOMETER_H //
+#endif // CHRONOMETER_H
---
+++ Projet/MeasureComplexity.h
@@ -0,0 +1,19 @@
+#ifndef MEASURE_COMPLEXITY_H
+#define MEASURE_COMPLEXITY_H
+
+// Pour chacune des fonctions qu'on a crée, nous allons créer une méthode qui prendra en entrée :
+/*  - Un entier qui représentera le nombre de fois qu'on exécutera la fonction
+    - Un nom de fichier qui correspondra au fichier ou seront écrit les données récoltés
+
+    Les méthodes crées auront pour but de mesurer le temps d'exécution de chacune de nos fonctions.
+    Les Matrices utilisées pour la mesures devront être crées aleatoirement comme demandé dans l'énoncé du projet.
+    On convertira en CSV les résultats directement via une méthode de Chronometre (vous pouvez en créer d'autres si celle déjà implémenter ne vous convient pas.)
+    On pourra par la suite tracer des graphes avec python ou ce que vous voulez. (Il exsite un dossier Mesures où pourront être stocker les résultats pour éviter de créer un bordel)
+*/
+
+// Mesure le temps d'exécution de la multiplication naive
+void measure_NaiveMatrixMultiplication(int n, char* fileName) {
+    //
+}
+
+#endif // MEASURE_COMPLEXITY_H
